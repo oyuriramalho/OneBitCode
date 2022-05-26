6f6eed2e4e8daa4ded11e6694d170ed779be9db1
Yuri Ramalho Notebook <oimplementador@gmail.com>
2022-05-25 22:16:45 -0300

    Iniciando Ingaia

---
+++ JS/inGaia/equities.js
@@ -0,0 +1,5 @@
+// Gestão de imóveis via DOM
+
+class Equities {
+  static register(element) {}
+}
--- JS/newHome.js
+++ JS/newHome.js
@@ -1,23 +1,27 @@
-function addNewHouse() {
-  let element = document.getElementsByTagName('input')
-  let line = document.createElement('tr')
-  for (let i = 0; i <= element.length - 1; i++) {
-    let item = document.createElement('td')
-    item.innerText = element[i].value
-    line.appendChild(item)
-  }
-  let item = document.createElement('td')
-  let innerItem = document.createElement('button')
-  innerItem.setAttribute('onclick', 'removeLine(this)')
-  innerItem.setAttribute('type', 'button')
-  innerItem.innerText = 'Remover'
-  item.appendChild(innerItem)
-  line.appendChild(item)
-  let T = document.getElementById("tableItems")
-  T.appendChild(line)
-}
+class inGaia {
+  static addNewHouse(X) {
+    let element = X.parentElement.getElementsByTagName('input')
+    let line = document.createElement('tr')
+    for (let i = 0; i <= element.length - 1; i++) {
+      let item = document.createElement('td')
+      item.innerText = element[i].value
+      element[i].value = ''
+      line.appendChild(item)
+    }
 
-function removeLine(element) {
-  var X = element
-  
+    let item = document.createElement('td')
+    let innerItem = document.createElement('button')
+    innerItem.setAttribute('onclick', 'inGaia.removeLine(this)')
+    innerItem.setAttribute('type', 'button')
+    innerItem.innerText = 'Remover'
+    item.appendChild(innerItem)
+    line.appendChild(item)
+    document.getElementById('tableItems').appendChild(line)
+  }
+
+  static removeLine(element) {
+    element.parentNode.parentNode.parentNode.removeChild(
+      element.parentNode.parentNode
+    )
+  }
 }
