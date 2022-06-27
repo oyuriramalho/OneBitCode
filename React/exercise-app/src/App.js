2a197c2a8610d9ea59833aff90bef1cac1a1ffb4
Yuri Ramalho Notebook <oimplementador@gmail.com>
2022-06-26 22:46:04 -0300

    Playing

---
+++ React/exercise-app/src/App.js
@@ -0,0 +1,12 @@
+
+function App() {
+  return (
+    <div>
+      <h1>Primeiro teste</h1>
+      <hr></hr>
+      
+    </div>
+  );
+}
+
+export default App;
---
+++ React/exercise-app/src/components/planetlist.js
@@ -0,0 +1,2 @@
+import {Fragment} from 'react'
+
