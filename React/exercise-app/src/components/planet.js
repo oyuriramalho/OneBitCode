33c7eb394aac8415bd4dee19b3aef32063519c10
Yuri Ramalho Notebook <oimplementador@gmail.com>
2022-06-27 22:04:42 -0300

    creating React Component with props

---
+++ React/exercise-app/src/components/planet.js
@@ -0,0 +1,14 @@
+import { Fragment } from 'react'
+
+const Planet = props => {
+  return(
+  <Fragment>
+    <h2>{props.name}</h2>
+    <p>{props.description}</p>
+    <a href={props.links}>Saiba mais</a>
+    <br></br>
+  </Fragment>
+)
+}
+
+export default Planet
