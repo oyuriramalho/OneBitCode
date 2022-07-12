831d41a6cd4945c4f6daaf48c026f27200afeb6e
Yuri Ramalho Notebook <oimplementador@gmail.com>
2022-07-11 21:43:24 -0300

    Interacting with React

--- React/exercise-app/src/components/planet.js
+++ React/exercise-app/src/components/planet.js
@@ -1,14 +1,19 @@
-import { Fragment } from 'react'
-
+// import { Fragment } from 'react'
+import './planet.css'
 const Planet = props => {
-  return(
-  <Fragment>
-    <h2>{props.name}</h2>
-    <p>{props.description}</p>
-    <a href={props.links}>Saiba mais</a>
-    <br/>
-  </Fragment>
-)
+  return (
+    <div onClick={() => props.clickOnPlanet}>
+      <h2>{props.name}</h2>
+      <p>{props.description}</p>
+      <p>
+        <a href={props.links} target="_blank" rel="noopener noreferrer">
+          Saiba mais
+        </a>
+      </p>
+      <img src={props.planetImg} alt={props.name} className="planet-img"></img>
+      <br />
+    </div>
+  )
 }
 
 export default Planet
