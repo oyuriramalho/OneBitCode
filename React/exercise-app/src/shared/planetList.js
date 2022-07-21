c2d0fd0d92467db2ca3237fe2487aab2a65229ad
Yuri Ramalho Notebook <oimplementador@gmail.com>
2022-07-20 22:54:26 -0300

    Another work day

--- React/exercise-app/src/shared/planetList.js
+++ React/exercise-app/src/shared/planetList.js
@@ -6,12 +6,18 @@ const clickOnPlanet = name => {
   alert(`Um clique no planeta ${name}`)
 }
 
+async function getPlanets(){
+  let response = await fetch('http://localhost:3000/api/planetlist.json')
+  let data = await response.json()
+  return data
+}
+
 class PlanetList extends React.Component {
   constructor(props) {
     super(props)
     this.state = {
       planets: [
-        {
+        {/*
           name: 'Terra',
           description:
             'A Terra é o terceiro planeta mais próximo do Sol, o mais denso e o quinto maior dos oito planetas do Sistema Solar. É também o maior dos quatro planetas telúricos. É por vezes designada como Mundo ou Planeta Azul. Lar de milhões de espécies de seres vivos,[13] incluindo os humanos, a Terra é o único corpo celeste onde é conhecida a existência de vida',
@@ -32,8 +38,16 @@ class PlanetList extends React.Component {
           links: 'https://pt.wikipedia.org/wiki/Netuno_(planeta)',
           theClick: () => clickOnPlanet(this.name),
           grey: true
-        }
+        */}
       ]
+    }
+
+    componentDidMount(){
+      getPlanets().then(data =>{
+        this.setState(state =>{
+          planets: data['planets']
+        })
+      })
     }
   }
 
@@ -48,7 +62,7 @@ class PlanetList extends React.Component {
             description={planet.description}
             planetImg={planet.planetImg}
             links={planet.links}
-            theClick={planet.theClick}
+            theClick={() => clickOnPlanet(planet.name)}
             grey={planet.grey}
           />
         ))}
